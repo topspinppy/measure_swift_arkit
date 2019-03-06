b563f4fc34265721fca672222e6947db20d1c980
Admin <admin@MAC4.local>
2019-03-06 15:33:48 +0700

    First Commit

--- measure/ViewController.swift
+++ measure/ViewController.swift
@@ -13,68 +13,144 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+	var center : CGPoint!
+	
+	let arrow = SCNScene(named: "art.scnassets/arrow.scn")!.rootNode
+	
+
+	var positions = [SCNVector3]()
+	
+	func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+		let hitTest = sceneView.hitTest(center, types: .featurePoint)
+		let result = hitTest.last
+		guard let transform = result?.worldTransform else {return}
+		let thirdColumn = transform.columns.3
+		let position = SCNVector3Make(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+		positions.append(position)
+		let lastTenPositions = positions.suffix(10)
+		arrow.position = getAveragePosition(from: lastTenPositions)
+	}
+
+	func getAveragePosition(from positions : ArraySlice<SCNVector3>) -> SCNVector3 {
+		var averageX : Float = 0
+		var averageY : Float = 0
+		var averageZ : Float = 0
+		
+		for position in positions {
+			averageX += position.x
+			averageY += position.y
+			averageZ += position.z
+		}
+		let count = Float(positions.count)
+		return SCNVector3Make(averageX / count , averageY / count, averageZ / count)
+	}
+	
+	
+	var isFirstPoint = true
+	var points = [SCNNode]()
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		
+		let sphereGeometry = SCNSphere(radius: 0.005)
+		let sphereNode = SCNNode(geometry: sphereGeometry)
+		sphereNode.position = arrow.position
+		sceneView.scene.rootNode.addChildNode(sphereNode)
+		points.append(sphereNode)
+		
+		if isFirstPoint {
+			isFirstPoint = false
+		} else {
+			//calculate the distance
+			let pointA = points[points.count - 2]
+			guard let pointB = points.last else {return}
+			
+			let d = distance(float3(pointA.position), float3(pointB.position))
+			
+			// add line
+			let line = SCNGeometry.line(from: pointA.position, to: pointB.position)
+			print(d.description)
+			let lineNode = SCNNode(geometry: line)
+			sceneView.scene.rootNode.addChildNode(lineNode)
+			
+			// add midPoint
+			let midPoint = (float3(pointA.position) + float3(pointB.position)) / 2
+			let midPointGeometry = SCNSphere(radius: 0.003)
+			midPointGeometry.firstMaterial?.diffuse.contents = UIColor.red
+			let midPointNode = SCNNode(geometry: midPointGeometry)
+			midPointNode.position = SCNVector3Make(midPoint.x, midPoint.y, midPoint.z)
+			sceneView.scene.rootNode.addChildNode(midPointNode)
+			
+			// add text
+			
+			let textGeometry = SCNText(string: String(format: "%.0f", d * 100) + "cm" , extrusionDepth: 1)
+			let textNode = SCNNode(geometry: textGeometry)
+			textNode.scale = SCNVector3Make(0.005, 0.005, 0.01)
+			textGeometry.flatness = 0.2
+			midPointNode.addChildNode(textNode)
+			
+			
+			// Billboard contraints
+			let contraints = SCNBillboardConstraint()
+			contraints.freeAxes = .all
+			midPointNode.constraints = [contraints]
+		
+			
+			isFirstPoint = true
+		}
+		
+		
+	}
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+		center = view.center
+		sceneView.scene.rootNode.addChildNode(arrow)
+		sceneView.autoenablesDefaultLighting = true
     }
-    
+	
+	override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+		center = view.center
+	}
+	
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
 }
+
+extension SCNGeometry {
+	class func line(from vectorA : SCNVector3, to vectorB : SCNVector3) -> SCNGeometry {
+		let indices : [Int32] = [0,1]
+		let source = SCNGeometrySource(vertices: [vectorA, vectorB])
+		let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+		return SCNGeometry(sources: [source], elements: [element])
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
